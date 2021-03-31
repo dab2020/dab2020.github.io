9559aae4749f6c4b7af984b865532f7e3cdd878c
dab2020 <dayyan.8661@beaconite.edu.pk>
2021-03-31 12:51:16 +0500

    Updates

---
+++ precache-manifest.b50a1c4f4664576b2c17dffaed0b1185.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14158185231ceb9ea160d34a3c673eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "540bf6b4ca7cb82b57d6",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540bf6b4ca7cb82b57d6",
+    "url": "/static/js/main.9e2bb543.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "57be341e2cb79ba188793df9153ef5db",
+    "url": "/static/media/hashirshoaeb.57be341e.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
